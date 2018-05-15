37cbdbb28759030f5bc8c045bd4d7a2e08d8dc37
Your Name <you@example.com>
2018-05-15 08:22:23 -0500

    Got month working :

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from collections import OrderedDict
 import records
 
 app = Flask(__name__)
@@ -8,8 +9,8 @@ def hello():
     db = records.Database('postgresql://schuna05:@knuth.luther.edu/movies')
     res = db.query('select distinct country from release_date order by country')
     res2 = db.query('select distinct year from release_date order by year')
-    #res3 = {'Jan':1,'Feb':2,'Mar':3,'Apr':4,'May':5,'June':6,'July':7,'Aug':8,'Sep':9,'Oct':10,'Nov':11,'Dec':12}
-    res3 = {'Jan':'Jan','Feb':'Feb','Mar':'Mar','Apr':'Apr','May':'May','June':'June','July':'July','Aug':'Aug','Sep':'Sep','Oct':'Oct','Nov':'Nov','Dec':'Dec'}
+    
+    res3 = OrderedDict([('Jan','Jan'),('Feb','Feb'),('Mar','Mar'),('Apr','Apr'),('May','May'),('June','June'),('July','July'),('Aug','Aug'),('Sep','Sep'),('Oct','Oct'),('Nov','Nov'),('Dec','Dec')])
     return render_template('index.html', countries=res,years=res2,months=res3 )
 
 @app.route('/processform')
@@ -17,9 +18,10 @@ def procform():
     countryx = request.args.get("country")
     yearx = request.args.get("year")
     monthx = request.args.get("month")
+    res3 = {'Jan':1,'Feb':2,'Mar':3,'Apr':4,'May':5,'June':6,'July':7,'Aug':8,'Sep':9,'Oct':10,'Nov':11,'Dec':12}
     db = records.Database('postgresql://schuna05:@knuth.luther.edu/movies')
-    res = db.query('select * from release_date where country = :country and year = :year limit 10', country = countryx, year =yearx)
-
+    res = db.query('select * from release_date where country = :country and year = :year and month = :month limit 10', country = countryx, year =yearx, month = res3[monthx])
+    
     return render_template('result.html', movress=res)
 
 
