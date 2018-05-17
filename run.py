d0989eb9545b2e4e694c0e66088f59c27de673f2
Anna <anna@anna-virtual-machine>
2018-05-16 20:08:24 -0500

    removing wtforms

--- run.py
+++ run.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, request
 from collections import OrderedDict
-from flask_wtf import Form
-from wtforms import StringField, PasswordField
 import records
 
 app = Flask(__name__)
